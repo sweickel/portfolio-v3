40cdb80340fdde2d10b591a19696f4c44a8cff7a
Sean Weickel <sweickel@gmail.com>
2025-06-29 21:53:35 -0400

    undo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/portfolio-v3/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
